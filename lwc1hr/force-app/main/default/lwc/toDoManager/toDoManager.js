5aeeb0a36174bbf78e2591e11ae06dfd67ac09ab
99s <divi9e@gmail.com>
2022-03-18 14:25:10 +0530

    lwc init
    before component

---
+++ lwc1hr/force-app/main/default/lwc/toDoManager/toDoManager.js
@@ -0,0 +1,84 @@
+import { LightningElement, track } from 'lwc';
+
+export default class ToDoManager extends LightningElement {
+    //but it works without track as well
+    @track _time = null;
+    @track _greeting = null;
+    @track todoS = [];
+
+     connectedCallback(){
+        this.timeNow();
+
+        setInterval(() => {
+            this.timeNow();
+            console.log('setIntervalrunning '+ this._time);
+        },1000*1)
+     }
+
+
+    timeNow(){
+        const date = new Date();
+         
+         var hour = date.getHours();
+         var minute = date.getMinutes();
+         this._time = date.toLocaleTimeString();
+         //this._time = `${this.getHours(hour)}:${this.getdoubleDigit(minute)}  ${this.getAMPM(hour)}`;
+
+         this.setGreeting(hour);
+    }
+
+    getHours(h){
+            return h===0 ? 12 : (h > 12 ? (h-12) : h);
+    }
+
+    getAMPM(h){
+        return h >= 12 ? 'PM' : 'AM';
+    
+    }
+
+    getdoubleDigit(d){
+        return (d < 10) ? "0"+d : d;
+    }
+
+
+    setGreeting(h){
+        if(h<12){
+            this._greeting = 'Guuuuoooooouuud Moarrning!';
+
+        }else if(h >= 12 && h < 17){
+            this._greeting = "Good Afternoon :]";
+        }
+        else{
+            this._greeting = "Good Night zzzzzz!";
+        }
+    }
+
+    /*Todo part */
+
+    addTodoHandler(){
+        const inputBox = this.template.querySelector('lightning-input');
+        console.log('added todo ',inputBox.value);
+        
+        const todoItem = {
+            todoId : this.todoS.length,
+            todoName : inputBox.value,
+            done : false,
+            todoDate: new Date()
+        }
+
+        this.todoS.push(todoItem);
+        inputBox.value = "";
+    }
+
+    get upcomingTasks(){
+        return this.todoS && this.todoS.length 
+        ? this.todoS.filter( todo => !todo.done) 
+        : [];
+    }
+
+    get completedTasks(){
+        return this.todoS && this.todoS.length 
+        ? this.todoS.filter( todo => todo.done) 
+        : [];
+    }
+}
---
+++ lwc1hr/force-app/main/default/lwc/todoItemComponent/todoItemComponent.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class TodoItemComponent extends LightningElement {}
